d052f61248e66087b9b288c2ef43c88a1a4d53d6
MaksimVardev21 <101968779+MaksimVardev21@users.noreply.github.com>
2023-11-26 20:45:12 +0200

    Add files via upload

---
+++ 05.+Substring_Грешно...cs
@@ -0,0 +1,40 @@
+﻿namespace Debugging_Substring
+{
+    using System;
+
+    public class Substring_broken
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            const char Search = 'р';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int endIndex = jump;
+
+                    if (endIndex > text.Length)
+                    {
+                        endIndex = text.Length;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex+1);
+                    Console.WriteLine(matchedString);
+                    i = endIndex+1;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
